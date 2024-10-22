c698a948064c47e14fa45469d7574d89261387b0
Parthasarathy <parthasarathyvenkidusamym@gmail.com>
2024-10-22 16:09:14 +0530

    Adding files via Git

---
+++ script.js
@@ -0,0 +1,16 @@
+var tablinks = document.getElementsByClassName('tab-links')
+var tabcontents = document.getElementsByClassName('tab-contents')
+
+
+function opentab(tab){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tab).classList.add("active-tab")
+}
